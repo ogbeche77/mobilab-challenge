8c44efab43866e5d3123cbec4f83055ac9d2b64f
ogbeche77 <ogbeche77@yahoo.com>
2020-10-11 17:08:34 +0200

    Typo fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-<<<<<<< HEAD
-      <p> Hellos </p>
-=======
-      <p> Hello World </p>
->>>>>>> 5e634a941ea8f70bb97828c4fa8f5b1c04bb377c
-    </div>
+    <div> <p> Hello </p></div>
   );
 }
 
