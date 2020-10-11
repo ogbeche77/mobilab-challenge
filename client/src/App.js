3e57d810cfceb90fbea266e4cbf7e7b630476d8d
ogbeche77 <ogbeche77@yahoo.com>
2020-10-11 17:07:20 +0200

    first

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+<<<<<<< HEAD
+      <p> Hellos </p>
+=======
+      <p> Hello World </p>
+>>>>>>> 5e634a941ea8f70bb97828c4fa8f5b1c04bb377c
+    </div>
+  );
+}
+
+export default App;
