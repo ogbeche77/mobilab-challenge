5808e49c5dbd27b624b93ec9b395d89a8e484c70
ogbeche77 <ogbeche77@yahoo.com>
2020-10-12 12:44:06 +0200

    Backend service

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-//Server implentation goes here
+
+const express = require("express");
+const displayApi = require("./DisplayApi.js");
+const cors = require("cors");
+const app = express(),
+    DIST_DIR = __dirname;
+app.use(express.static(DIST_DIR));
+app.use(cors());
+app.use(express.json());
+displayApi.api(app);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`);
+
+});
