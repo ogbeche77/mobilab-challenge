42989fcbbfb61402cc776653078fbaef72961cac
ogbeche77 <ogbeche77@yahoo.com>
2020-10-13 11:26:30 +0200

    Api authenticated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const baseUrl = "http://localhost:8000";
 exports.instance = axios.create({
     baseURL: baseUrl,
-    timeout: 300000,
     headers: {
         "content-type": "application/json",
         Authorization: "Client-ID cec8e84a5992a0f",
--- server/api.js
+++ server/api.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const baseUrl = "https://api.imgur.com/3/";
 exports.instance = axios.create({
     baseURL: baseUrl,
-    timeout: 300000,
     headers: {
         "content-type": "application/json",
         Authorization: "Client-ID cec8e84a5992a0f",
